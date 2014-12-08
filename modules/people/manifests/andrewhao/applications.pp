8ec9ad608613c61c0a122dd1c9cb95dfeea0dd46
Andrew Hao <andrewhao@gmail.com>
2014-12-07 22:57:44 -0800

    Adding vim

--- modules/people/manifests/andrewhao/applications.pp
+++ modules/people/manifests/andrewhao/applications.pp
@@ -2,8 +2,12 @@ class people::andrewhao::applications {
   include iterm2::stable
   include dropbox
   include virtualbox
-  #include vagrant
+  include vagrant
   include chrome
   include silver_searcher
+  include ohmyzsh
+  include vim
+  #include macvim
+  include janus
 }
 
